ff35bccb6fdf49794df73ec7ebc504edb4455fa0
Stephen Muñez <sjtmunez@gmail.com>
2023-10-27 01:04:39 +0800

    setup: colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        '050505': '#050505',
+        '1F1F1F': '#1F1F1F',
+        '2D2D2D': '#2D2D2D',
+        '3A3A3A': '#3A3A3A',
+        757575: '#757575',
+        E9E9E9: '#E9E9E9',
+        F4F4F4: '#F4F4F4',
+        FFFFFF: '#FFFFFF',
+        A445ED: '#A445ED',
+        FF5252: 'FF5252',
+      },
+    },
   },
   plugins: [],
 };
