a119f9121031e8468dd14db0b8c8d4d7324d9e4b
Stephen Muñez <sjtmunez@gmail.com>
2023-10-29 04:08:58 +0800

    feat: api composable

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <template>
   <div>Dictionary Web App</div>
+
+  <div v-if="loading">Loading...</div>
+  <div v-else-if="error">{{ error }}</div>
+  <div v-else-if="definition">
+    <h2>{{ definition.word }}</h2>
+  </div>
+  <div v-else>No definition found.</div>
 </template>
 
 <script>
-export default {};
+import getDefinition from './composables/getDefinition';
+export default {
+  setup() {
+    const { definition, error, loading, searchWord } = getDefinition();
+
+    searchWord('hello');
+
+    return { definition, error, loading, searchWord };
+  },
+};
 </script>
